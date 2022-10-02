b415f514543fb28e35b09b10863c3304b921f1b5
NJKatorz <nathanesakwa@gmail.com>
2022-10-02 23:29:01 +0200

    ui

--- exercices/module2/2.3/src/index.js
+++ exercices/module2/2.3/src/index.js
@@ -1,2 +1,13 @@
+/* eslint-disable no-var */
+/* eslint-disable no-alert */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+getValue();
+function getValue(){
+    var line = document.getElementById('oui');
+    alert("Valeur = ", line.value);
+}
+
+//      <button type="submit" onclick="getValue();" class="btn btn-primary">Create table</button>
+//      <input type="submit" value="Create table" onclick="getValue();" class="btn btn-primary">
