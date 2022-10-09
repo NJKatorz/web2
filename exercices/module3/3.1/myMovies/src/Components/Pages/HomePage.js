dc8908abd26975a4a71a6cd6137efeeacc47b492
NJKatorz <nathanesakwa@gmail.com>
2022-10-09 20:25:15 +0200

    ok

---
+++ exercices/module3/3.1/myMovies/src/Components/Pages/HomePage.js
@@ -0,0 +1,31 @@
+const HomePage = () => {
+   /**  const main = document.querySelector('main');
+    // main.innerHTML = 'Deal with the content of your HomePage';
+   
+    const mainTable = `
+    <div>
+    <table border="0">
+      <tr>
+          <td>Title</td>
+          <td>Duration (min)</td>
+          <td>Budget (million)</td>
+      </tr>
+      <tr>
+          <td>Harry Potter and the Philosopher's Stone</td>
+          <td>152</td>
+          <td>125</td>
+      </tr>
+      <tr>
+          <td>Avengers: Endgame</td>
+          <td>181</td>
+          <td>181</td>
+      </tr>
+    </table>
+    </div>`;
+    main.innerHTML = mainTable;
+*/    
+
+
+};
+
+export default HomePage;
