6ba6d4ee401b6b50d1eaaaebbfaebbd76149d5ba
NJKatorz <nathanesakwa@gmail.com>
2022-11-18 15:03:53 +0100

    myMovies : module 7 : safe api

--- exercices/module7/7.1/hmi/webpack.config.js
+++ exercices/module7/7.1/hmi/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: `${__dirname}/dist`,
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
   },
   devtool: 'eval-source-map',
   devServer: {
